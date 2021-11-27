677fde2ad54bda66f68c4c0abc5f46342206f085
rechavar <rechavar@eafit.edu.co>
2021-11-27 16:14:55 -0500

    deployment

--- 06-deploy_model.py
+++ 06-deploy_model.py
@@ -1,4 +1,14 @@
 from azureml.core.webservice import Webservice
 from azureml.core.model import InferenceConfig, Model
 from azureml.core.environment import Environment
-from azureml.core import Workspace
+from azureml.core import Workspace, Workspace
+
+ws = Workspace.from_config(path='./.azureml', _file_name='config.json')
+
+model = Model(ws,name ='pyTennis', version=2)
+
+env = Environment.from_pip_requirements(
+    name='pytennis-aml-env',
+    file_path='./.azureml/requirements.txt'
+)
+
